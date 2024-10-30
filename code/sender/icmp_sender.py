d3ea90ce174fb054b27804002b315194c8e90cb1
merteren1234 <118080184+merteren1234@users.noreply.github.com>
2024-10-30 19:24:14 +0300

    Update icmp_sender.py

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,14 @@
 import scapy
+import scapy.all
 
+class icmp_sender:
+    def __init__(self):
+        self.sentPacket()
+
+
+    def sentPacket(self):
+        pack=scapy.all.IP(dst="172.21.0.2",ttl=1)/scapy.all.ICMP()
+        scapy.all.send(pack)
+
+icmp_sender()
 # Implement your ICMP sender here
